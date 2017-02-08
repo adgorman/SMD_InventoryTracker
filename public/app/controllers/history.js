01b70e65a04a879c2d5e6b6ca4f2934d6a2fe84b
Adam Gorman <Adam@Adams-MacBook-Pro.local>
2017-02-08 16:35:39 -0500

    Added Delete History Button
    - There is an X in the corner of each history log to delete it
    - When a list is deleted, item quantities are returned to what they
    were before the list was created

--- public/app/controllers/history.js
+++ public/app/controllers/history.js
@@ -8,16 +8,35 @@
         .module('app')
         .controller('History', History);
 
-    History.$inject = ['applicationData', 'firebaseDataService', '$scope'];
+    History.$inject = ['applicationData', 'firebaseDataService', '$q', '$scope'];
 
-    function History(applicationData, firebaseDataService, $scope) {
+    function History(applicationData, firebaseDataService, $q, $scope) {
         var vm = this;
         vm.items = applicationData.items;
         vm.logList = [];
+        vm.removeList = removeList;
 
         activate();
 
         function activate() {
+        }
+
+        function removeList(log) {
+            var promises = [firebaseDataService.deleteHistoryList(log.$key)];
+            _.each(log.itemList, function(item) {
+                var updatedItem = {
+                    name: vm.items[item.itemID].name,
+                    quantity: vm.items[item.itemID].quantity + item.quantity,
+                    storageAreaID: vm.items[item.itemID].storageAreaID
+                };
+                promises.push(firebaseDataService.setItem(item.itemID, updatedItem).then(function() {
+                    applicationData.items[item.itemID].quantity = updatedItem.quantity;
+                }));
+            });
+
+            $q.all(promises).then(function () {
+                delete applicationData.historyLists[log.$key];
+            });
         }
 
         $scope.$watch(function() { return applicationData.serviceInitialized; }, function(initialized) {
@@ -30,6 +49,7 @@
         $scope.$watch(function() { return applicationData.historyLists; }, function(historyLists) {
             vm.logList = applicationData.historyLists;
             formatLogList(vm.logList);
+
             function formatLogList(logList) {
                 logList = _.mapObject(logList, function(log) {
                     var date = new Date(log.date);
